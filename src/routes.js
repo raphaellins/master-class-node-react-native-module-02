cb656df1d503a2fe166fbb4df42ce4e7a6a3078c
Raphael lins <raphalinns@gmail.com>
2019-08-31 21:46:56 -0300

    creating others entities with foreign keys; file upload

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,24 @@
 import {Router} from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController'
 import SessionController from '../app/controllers/SessionController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
 
 routes.use(authMiddleware);
 
-routes.put('/users', UserController.update)
+routes.put('/users', UserController.update);
+
+routes.post('/files', FileController.store);
+
+routes.get('providers', ProviderController.find)
 
 export default routes;
